35ced42b497efde7c44ce02fd3f0b5205559a6d1
Miuura <valentinohartanto@gmail.com>
2024-08-15 11:35:19 +0700

    add some decorations

--- app.py
+++ app.py
@@ -60,27 +60,29 @@ knn1.fit(X1_train, y1_train)
 KNNtuned1 = KNeighborsClassifier(leaf_size=5,metric='manhattan',n_neighbors=3,weights='uniform')
 KNNtuned1.fit(X1_train, y1_train)
 
-st.title('Prediction Sirtuin6 Small Molecules')
+st.set_page_config(page_title="Tugas Besar Pengenalan Kecerdasan Buatan", page_icon=":sparkles:")
+
+st.title(':blue[Prediction Sirtuin6 Molecules] 🔬')
 st.markdown("""
-Tugas Besar PKB Kelompok 1:
+### :books: Tugas Besar PKB Kelompok 1:
 - Valentino Hartanto 1301223020
 - Gede Bagus Krishnanditya Merta 1301223088 
 - Raka Aditya Waluya 1301220192
 
-Deskripsi: 
+### :page_facing_up: Deskripsi: 
 \nModel dibangun dengan tujuan untuk mengklasifikasikan molekul menjadi dua kategori High BFE dan Low BFE berdasarkan parameter yang relevan. Dengan demikian, model ini akan membantu dalam pemilihan molekul-molekul yang memiliki potensi sebagai kandidat inhibitor protein target.
 """)
 st.write('')
 
-SC5 = st.number_input('Masukkan nilai SC-5: ', value=0.0, step=0.00005, format='%.5f')
-SP6 = st.number_input('Masukkan nilai SP-6: ', value=0.0, step=0.00005, format='%.5f')
-SHBd = st.number_input('Masukkan nilai SHBd: ', value=0.0, step=0.00005, format='%.5f')
-minHaaCH = st.number_input('Masukkan nilai minHaaCH: ', value=0.0, step=0.00005, format='%.5f')
-maxwHBa = st.number_input('Masukkan nilai maxwHBa: ', value=0.0, step=0.00005, format='%.5f')
-FMF = st.number_input('Masukkan nilai FMF: ', value=0.0, step=0.00005, format='%.5f')
+SC5 = st.number_input('🔢 Masukkan nilai SC-5: ', value=0.0, step=0.00005, format='%.5f')
+SP6 = st.number_input('🔢 Masukkan nilai SP-6: ', value=0.0, step=0.00005, format='%.5f')
+SHBd = st.number_input('🔢 Masukkan nilai SHBd: ', value=0.0, step=0.00005, format='%.5f')
+minHaaCH = st.number_input('🔢 Masukkan nilai minHaaCH: ', value=0.0, step=0.00005, format='%.5f')
+maxwHBa = st.number_input('🔢 Masukkan nilai maxwHBa: ', value=0.0, step=0.00005, format='%.5f')
+FMF = st.number_input('🔢 Masukkan nilai FMF: ', value=0.0, step=0.00005, format='%.5f')
 
 st.write('')
-if st.button('Predict Class'):
+if st.button('🔍 Predict Class'):
     data = pd.DataFrame({
     'SC-5': [SC5],
     'SP-6': [SP6],
@@ -99,14 +101,14 @@ if st.button('Predict Class'):
     y_pred_tuned_knn_unscaled = KNNtuned1.predict(data)
     
     st.write('')
-    st.write(f'**Decision Tree:** ')
+    st.write(f'**🌳 Decision Tree:** ')
     st.write('Prediksi Class menggunakan Decision Tree: ', y_pred_normal_tree[0].replace('_', ' '))
     st.write('Prediksi Class menggunakan Pruned Decision Tree: ', y_pred_prune_tree[0].replace('_', ' '))
     st.write('')
-    st.write(f'**KNN menggunakan Unscaled Data:** ')
+    st.write(f'**🔍 KNN menggunakan Unscaled Data:** ')
     st.write(f'Prediksi Class menggunakan KNN: ', y_pred_knn_unscaled[0].replace('_', ' '))
     st.write(f'Prediksi Class menggunakan Tuned KNN: ', y_pred_tuned_knn_unscaled[0].replace('_', ' '))
     st.write('')
-    st.write(f'**KNN menggunakan Scaled Data:** ')
+    st.write(f'**📈 KNN menggunakan Scaled Data:** ')
     st.write(f'Prediksi Class menggunakan KNN: ', y_pred_knn_scaled[0].replace('_', ' '))
     st.write(f'Prediksi Class menggunakan Tuned KNN: ', y_pred_tuned_knn_scaled[0].replace('_', ' '))
